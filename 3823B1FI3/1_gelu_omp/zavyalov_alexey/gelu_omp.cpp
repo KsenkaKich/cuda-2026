b9ae6f83e3839681f1a0abaadd6163ae2eb84f88
unknown <the_h1nk@mail.ru>
2026-03-02 15:12:37 +0300

    gelu omp - optimize for gcc

--- 3823B1FI3/1_gelu_omp/zavyalov_alexey/gelu_omp.cpp
+++ 3823B1FI3/1_gelu_omp/zavyalov_alexey/gelu_omp.cpp
@@ -7,58 +7,20 @@ std::vector<float> GeluOMP(const std::vector<float>& input) {
     float sqrt_2_div_pi = 0.7978845608028653558798921198687637369517172623298693153318516593f;
 
 #pragma omp parallel for
-    for (i = 0; i < n - 8; i += 8) {
-        float x = input[i];
-        float tanh_arg = sqrt_2_div_pi * (x + 0.044715f * x * x * x);
-        float exp_precalc = exp(2.0f * tanh_arg);
-        float cnst = (1.0f - 1.0f / (exp_precalc + 1.0f));
-        res[i] = x * cnst;
-        //res[i] = 0.5f * x * (1 + tanh(tanh_arg));
+    for (i = 0; i < n - 7; i += 8) {
 
-        x = input[i + 1];
-        tanh_arg = sqrt_2_div_pi * (x + 0.044715f * x * x * x);
-        exp_precalc = exp(2.0f * tanh_arg);
-        cnst = (1.0f - 1.0f / (exp_precalc + 1.0f));
-        res[i + 1] = x * cnst;
-
-        x = input[i + 2];
-        tanh_arg = sqrt_2_div_pi * (x + 0.044715f * x * x * x);
-        exp_precalc = exp(2.0f * tanh_arg);
-        cnst = (1.0f - 1.0f / (exp_precalc + 1.0f));
-        res[i + 2] = x * cnst;
-
-        x = input[i + 3];
-        tanh_arg = sqrt_2_div_pi * (x + 0.044715f * x * x * x);
-        exp_precalc = exp(2.0f * tanh_arg);
-        cnst = (1.0f - 1.0f / (exp_precalc + 1.0f));
-        res[i + 3] = x * cnst;
-
-        x = input[i + 4];
-        tanh_arg = sqrt_2_div_pi * (x + 0.044715f * x * x * x);
-        exp_precalc = exp(2.0f * tanh_arg);
-        cnst = (1.0f - 1.0f / (exp_precalc + 1.0f));
-        res[i + 4] = x * cnst;
-
-        x = input[i + 5];
-        tanh_arg = sqrt_2_div_pi * (x + 0.044715f * x * x * x);
-        exp_precalc = exp(2.0f * tanh_arg);
-        cnst = (1.0f - 1.0f / (exp_precalc + 1.0f));
-        res[i + 5] = x * cnst;
-
-        x = input[i + 6];
-        tanh_arg = sqrt_2_div_pi * (x + 0.044715f * x * x * x);
-        exp_precalc = exp(2.0f * tanh_arg);
-        cnst = (1.0f - 1.0f / (exp_precalc + 1.0f));
-        res[i + 6] = x * cnst;
-
-        x = input[i + 7];
-        tanh_arg = sqrt_2_div_pi * (x + 0.044715f * x * x * x);
-        exp_precalc = exp(2.0f * tanh_arg);
-        cnst = (1.0f - 1.0f / (exp_precalc + 1.0f));
-        res[i + 7] = x * cnst;
+        res[i] = input[i] * (1.0f - 1.0f / (exp(2.0f * sqrt_2_div_pi * (input[i] + 0.044715f * input[i] * input[i] * input[i])) + 1.0f));
+        res[i + 1] = input[i + 1] * (1.0f - 1.0f / (exp(2.0f * sqrt_2_div_pi * (input[i + 1] + 0.044715f * input[i + 1] * input[i + 1] * input[i + 1])) + 1.0f));
+        res[i + 2] = input[i + 2] * (1.0f - 1.0f / (exp(2.0f * sqrt_2_div_pi * (input[i + 2] + 0.044715f * input[i + 2] * input[i + 2] * input[i + 2])) + 1.0f));
+        res[i + 3] = input[i + 3] * (1.0f - 1.0f / (exp(2.0f * sqrt_2_div_pi * (input[i + 3] + 0.044715f * input[i + 3] * input[i + 3] * input[i + 3])) + 1.0f));
+        res[i + 4] = input[i + 4] * (1.0f - 1.0f / (exp(2.0f * sqrt_2_div_pi * (input[i + 4] + 0.044715f * input[i + 4] * input[i + 4] * input[i + 4])) + 1.0f));
+        res[i + 5] = input[i + 5] * (1.0f - 1.0f / (exp(2.0f * sqrt_2_div_pi * (input[i + 5] + 0.044715f * input[i + 5] * input[i + 5] * input[i + 5])) + 1.0f));
+        res[i + 6] = input[i + 6] * (1.0f - 1.0f / (exp(2.0f * sqrt_2_div_pi * (input[i + 6] + 0.044715f * input[i + 6] * input[i + 6] * input[i + 6])) + 1.0f));
+        res[i + 7] = input[i + 7] * (1.0f - 1.0f / (exp(2.0f * sqrt_2_div_pi * (input[i + 7] + 0.044715f * input[i + 7] * input[i + 7] * input[i + 7])) + 1.0f));
     }
 
-    for (int i = std::max(0, n - 16); i < n; i++) {
+    int ostalos = n % 8;
+    for (int i = std::max(0, n - ostalos); i < n; i++) {
         float x = input[i];
         float tanh_arg = sqrt_2_div_pi * (x + 0.044715f * x * x * x);
         float exp_precalc = exp(2.0f * tanh_arg);
