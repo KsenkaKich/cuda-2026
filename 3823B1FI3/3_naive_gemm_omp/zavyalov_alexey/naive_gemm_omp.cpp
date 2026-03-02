9a8604fefb4ec52fef63b1d6e9f0e6c9032f6a0f
unknown <the_h1nk@mail.ru>
2026-03-02 14:42:30 +0300

    moved files to correct folders

---
+++ 3823B1FI3/3_naive_gemm_omp/zavyalov_alexey/naive_gemm_omp.cpp
@@ -0,0 +1,36 @@
+#include "naive_gemm_omp.h"
+
+std::vector<float> NaiveGemmOMP(const std::vector<float>& a,
+    const std::vector<float>& b,
+    int n) {
+
+    std::vector<float> c(n * n);
+
+#pragma omp parallel for
+    for (int i = 0; i < n; ++i) {
+        for (int k = 0; k < n; ++k) {
+            int i_mult_n = i * n;
+            int k_mult_n = k * n;
+// #pragma omp simd
+            float a_element = a[i_mult_n + k];
+            for (int j = 0; j < n - 7; j += 8) {
+                c[i_mult_n + j] += a_element * b[k_mult_n + j];
+                c[i_mult_n + (j + 1)] += a_element * b[k_mult_n + (j + 1)];
+                c[i_mult_n + (j + 2)] += a_element * b[k_mult_n + (j + 2)];
+                c[i_mult_n + (j + 3)] += a_element * b[k_mult_n + (j + 3)];
+                c[i_mult_n + (j + 4)] += a_element * b[k_mult_n + (j + 4)];
+                c[i_mult_n + (j + 5)] += a_element * b[k_mult_n + (j + 5)];
+                c[i_mult_n + (j + 6)] += a_element * b[k_mult_n + (j + 6)];
+                c[i_mult_n + (j + 7)] += a_element * b[k_mult_n + (j + 7)];
+            }
+            
+            // хвосты
+            int ostalos = n % 8;
+            for (int j = n - ostalos; j < n; ++j) {
+                c[i_mult_n + j] += a_element * b[k_mult_n + j];
+            }
+        }
+    }
+
+    return c;
+}
