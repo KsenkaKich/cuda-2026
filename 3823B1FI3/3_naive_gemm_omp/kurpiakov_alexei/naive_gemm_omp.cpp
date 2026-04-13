51b4370ee1373fa02e75355ceec2f72c1e5eea49
AlKurpiakov <s23380350@unn.ru>
2026-04-13 19:27:14 +0300

    modified:   3823B1FI3/3_naive_gemm_omp/kurpiakov_alexei/naive_gemm_omp.cpp 	modified:   3823B1FI3/3_naive_gemm_omp/kurpiakov_alexei/naive_gemm_omp.h

--- 3823B1FI3/3_naive_gemm_omp/kurpiakov_alexei/naive_gemm_omp.cpp
+++ 3823B1FI3/3_naive_gemm_omp/kurpiakov_alexei/naive_gemm_omp.cpp
@@ -1,7 +1,9 @@
-#pragma GCC optimize("O3,fast-math")
+#pragma GCC optimize("O3,fast-math,unroll-loops")
 #include "naive_gemm_omp.h"
 
 
+#pragma GCC target("avx2")
+#pragma GCC target("fma")
 std::vector<float> NaiveGemmOMP(const std::vector<float>& a,
                                 const std::vector<float>& b,
                                 int n){
@@ -11,10 +13,12 @@ std::vector<float> NaiveGemmOMP(const std::vector<float>& a,
 #pragma omp parallel for schedule(static, 64)
     for (int i = 0; i < n; ++i){
         for (int j = 0; j < n; ++j){
+            
+            float a_i = a[i * n + j];
 
             #pragma omp simd
             for (int k = 0; k < n; ++k){
-                res[i * n + k] += a[i * n + j] * b[j * n + k];
+                res[i * n + k] += a_i * b[j * n + k];
             }
         
         }
--- 3823B1FI3/3_naive_gemm_omp/kurpiakov_alexei/naive_gemm_omp.h
+++ 3823B1FI3/3_naive_gemm_omp/kurpiakov_alexei/naive_gemm_omp.h
@@ -2,6 +2,7 @@
 #define __NAIVE_GEMM_OMP_H
 
 #include <vector>
+#include <omp.h>
 
 std::vector<float> NaiveGemmOMP(const std::vector<float>& a,
                                 const std::vector<float>& b,
