659c4a7cb94036e06a511b3c8a2c60be2cd769a1
fartekool <levonychev2006@gmail.com>
2026-04-26 23:32:08 +0300

    ready to work

---
+++ 3823B1FI3/6_block_gemm_cuda/levonychev_ivan/block_gemm_cuda.cu
@@ -0,0 +1,9 @@
+#include "block_gemm_cuda.h"
+#include <cuda_runtime.h>
+
+
+std::vector<float> BlockGemmCUDA(const std::vector<float>& a,
+                                 const std::vector<float>& b,
+                                 int n) {
+    // Place your implementation here
+}
