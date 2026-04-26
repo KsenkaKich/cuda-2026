74bd182da73556f0c61ef43cdefd353db4c28e31
DimaVotincev <dvotinccev@gmail.com>
2026-04-26 21:50:49 +0300

    block gemm omp speedup

--- 3823B1FI3/5_block_gemm_omp/votincev_dmitri/block_gemm_omp.cpp
+++ 3823B1FI3/5_block_gemm_omp/votincev_dmitri/block_gemm_omp.cpp
@@ -1,4 +1,4 @@
-#include "block_gemm_omp.h"
+#include "naive_gemm_omp.h"
 #include "omp.h"
 #include <vector>
 
@@ -21,44 +21,28 @@ std::vector<float> BlockGemmOMP(const std::vector<float>& a,
 
     std::vector<float> answer(n * n, 0.0f);
 
+
     // блочное умножение 
     const int bl_size = 64;
     const int bl_count = n / bl_size;
 
-    // + распараллеливаем внешние блоки 
-    // + склеивание 2х внешних циклов 
-    // + распределение итераций между потоками 
-    #pragma omp parallel for collapse(2) schedule(static)
+    // распараллеливаем внешние блоки
+#pragma omp parallel for collapse(2)
     for (int bl_j = 0; bl_j < bl_count; bl_j++) {
-        for (int bl_k = 0; bl_k < bl_count; bl_k++) {
-            for (int bl_i = 0; bl_i < bl_count; bl_i++) {
-
-                // j - определяет строку в a
-
+        for (int bl_i = 0; bl_i < bl_count; bl_i++) {
+            for (int bl_k = 0; bl_k < bl_count; bl_k++) {
                 for (int j = bl_j * bl_size; j < (bl_j + 1) * bl_size; j++) {
-                    // k - определяет элемент, который умножаем
-                    
+                    float* res_row = &answer[j * n];
+                    const float* a_row = &a[j * n];
                     for (int k = bl_k * bl_size; k < (bl_k + 1) * bl_size; k++) {
-
-                        // a[j * n + k] - не зависит от i
-                        // можно посчитать 1 раз (вынести из цикла по  i)
-                        float a_jk = a[j * n + k];
-
-                        // i - определяет столбец в b
+                        float a_val = a_row[k];
+                        const float* b_row = &b[k * n];
 
 
-                        // для разворачивания цикла по i
-                        int row_idx = j * n;
-                        int b_idx = k * n;
 
-                        // векторизация цикла
-                        #pragma omp simd
-                        for (int i = bl_i * bl_size; i < (bl_i + 1) * bl_size; i += 4) {
-                            // разворачивание цикла на 4
-                            answer[i + row_idx] += a_jk * b[i + b_idx];
-                            answer[i + 1 + row_idx] += a_jk * b[i + 1 + b_idx];
-                            answer[i + 2 + row_idx] += a_jk * b[i + 2 + b_idx];
-                            answer[i + 3 + row_idx] += a_jk * b[i + 3 + b_idx];
+#pragma omp simd
+                        for (int i = bl_i * bl_size; i < (bl_i + 1) * bl_size; ++i) {
+                            res_row[i] += a_val * b_row[i];
                         }
                     }
                 }
@@ -68,3 +52,4 @@ std::vector<float> BlockGemmOMP(const std::vector<float>& a,
 
     return answer;
 }
+
